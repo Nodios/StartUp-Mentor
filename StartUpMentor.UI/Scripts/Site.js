b25dad95ccd1e72cc1c18ac8582f032225ef5180
Dino Repac <drepac@etfos.hr>
2016-03-12 18:01:31 +0100

    garbage

---
+++ StartUpMentor.UI/Scripts/Site.js
@@ -0,0 +1,8 @@
+﻿$(window).on('scroll', function () {
+    if ($(window).scrollTop() > 100) {
+        $('.navbar').removeClass('navbar-top');
+    } else {
+        $('.navbar').addClass('navbar-top');
+    }
+
+});
