8b7feed9bd31423994efdb5afbe2f564d627dbea
Dino Repac <drepac@etfos.hr>
2016-03-12 18:02:26 +0100

    Revert "garbage"
    This reverts commit b25dad95ccd1e72cc1c18ac8582f032225ef5180.

--- StartUpMentor.UI/Scripts/Site.js
+++
@@ -1,8 +0,0 @@
-﻿$(window).on('scroll', function () {
-    if ($(window).scrollTop() > 100) {
-        $('.navbar').removeClass('navbar-top');
-    } else {
-        $('.navbar').addClass('navbar-top');
-    }
-
-});
