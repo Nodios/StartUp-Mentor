51f594d85806cdf68b4261dd4add2a115f800ea9
Dino Repac <drepac@etfos.hr>
2016-03-19 12:12:14 +0100

    added base style for home/about

--- StartUpMentor.UI/App_Start/BundleConfig.cs
+++ StartUpMentor.UI/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace StartUpMentor.UI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
