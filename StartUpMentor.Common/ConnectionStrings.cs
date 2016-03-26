e1d6345b4b74f299202475b748f4e0cf7ab9b28f
Dino Repac <drepac@etfos.hr>
2016-03-26 19:11:45 +0100

    styling fixes

--- StartUpMentor.Common/ConnectionStrings.cs
+++ StartUpMentor.Common/ConnectionStrings.cs
@@ -4,6 +4,7 @@
     {
 		//public const string CONNECTION = "StartUpMentorDbContext";
 		//public const string CONNECTION = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=TestStartUpMentor;Integrated Security=True;MultipleActiveResultSets=True";
-		public const string CONNECTION = "Data Source=.\\SQLEXPRESS;Initial Catalog = TestStartUpMentorContext; Integrated Security = True; MultipleActiveResultSets=True";
-		}
+		public const string CONNECTION_MAIN = "Data Source=.\\SQLEXPRESS;Initial Catalog = TestStartUpMentorContext; Integrated Security = True; MultipleActiveResultSets=True";
+        public const string CONNECTION = "Data Source=.\\DINO_SERVER;Initial Catalog = TestStartUpMentorContext; Integrated Security = True; MultipleActiveResultSets=True";
+    }
 }
