9ecae06aa53abfde4d85809bb6519511be179274
LukaLoina <LukaLoina@users.noreply.github.com>
2016-05-12 01:37:17 +0200

    Add QuestionAskUserModel.cs

---
+++ StartUpMentor.UI/Models/Question/QuestionAskUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StartUpMentor.UI.Models.Question
+{
+	public class QuestionAskUserModel
+	{
+        public Guid FieldId { get; set; }
+		public Guid UserId { get; set; }
+        public string Title { get; set; }
+		public string QuestionText { get; set; }
+		public IEnumerable<StartUpMentor.Model.Common.IField> FieldList { get; set; }
+    }
+}
